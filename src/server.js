7c6b1adaa5ee638a97f81a57864cdc940f97b315
bekmurod_yusupov <bekmurodyusupov81@gmail.com>
2024-07-07 22:13:06 +0500

    headers

--- src/server.js
+++ src/server.js
@@ -1,19 +1,14 @@
 import express from "express";
 import cors from "cors";
 import "./db/mongodb.js";
-import fileUpload from "express-fileupload";
+import headerRouter from "./router/header.router.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(fileUpload({
-    limits: { fileSize: 15 * 1024 * 1024 },
-}));
-//app.use(express.static("uploads"));
 
+app.use("/headers", headerRouter);
 
-//app.use("/workplaces", express.static("/workplaces"));
 app.listen(PORT);
 console.log("server listening " + PORT);
