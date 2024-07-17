7ff0cf4328e4c4087ec939f8bf91646e14fdd508
bekmurod_yusupov <bekmurodyusupov81@gmail.com>
2024-07-17 15:04:25 +0500

    headers

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "./db/mongodb.js";
 
-import bgRouter from "./router/bg.router.js";
+import headerRouter from "./router/bg.router.js";
 import productRouter from "./router/productF.router.js";
 import bgRouter from "./router/bg.router.js";
 import editRouter from "./router/editorial.router.js";
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use("/headers", bgRouter);
+app.use("/headers", headerRouter);
 app.use("/product-features", productRouter);
 app.use("/bgImage", bgRouter);
 app.use("/editorial", editRouter);
